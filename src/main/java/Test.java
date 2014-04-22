8fd95b74e9ce72ed148357eea6db826d3ef19baf
demoth <demoth.cadaver@gmail.com>
2014-04-22 19:41:25 +0400

    package reorder

---
+++ src/main/java/Test.java
@@ -0,0 +1,13 @@
+import client.ClientMain;
+import server.ServerMain;
+
+/**
+ * Created by daniil on 10.04.14.
+ */
+public class Test {
+    public static void main(String... args) throws InterruptedException {
+        ServerMain.main();
+        Thread.sleep(1342);
+        ClientMain.main();
+    }
+}
