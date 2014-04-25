66a550c3f3202e3bde0397202d3ebba21a95a214
demoth <demoth.cadaver@gmail.com>
2014-04-25 23:59:23 +0400

    added config setters

---
+++ src/main/java/TestConfig.java
@@ -0,0 +1,31 @@
+import com.jme3.math.Vector3f;
+import common.Config;
+
+import java.util.logging.Logger;
+
+/**
+ * Created by demoth on 25.04.14.
+ */
+public class TestConfig {
+    public static void main(String[] args) {
+        Logger log = Logger.getLogger("TestConfig");
+        log.info("---INTEGER TEST---");
+        log.info("sv_port:" + Config.sv_port);
+        Config.setters.get("sv_port").set("3214");
+        log.info("new sv_port: " + Config.sv_port);
+        assert Config.sv_port == 3214;
+
+        log.info("---   LONG TEST---");
+        log.info("sv_sleep:" + Config.sv_sleep);
+        Config.setters.get("sv_sleep").set("1234125");
+        log.info("new sv_sleep: " + Config.sv_sleep);
+        assert Config.sv_sleep == 1234125;
+
+        log.info("--- VECTOR TEST---");
+        log.info("g_spawn_point: " + Config.g_spawn_point);
+        Config.setters.get("g_spawn_point").set("1 2 3");
+        log.info("new g_spawn_point: " + Config.g_spawn_point);
+        assert new Vector3f(1f, 2f, 3f).equals(Config.g_spawn_point);
+
+    }
+}
--- src/main/java/common/Config.java
+++ src/main/java/common/Config.java
@@ -2,6 +2,10 @@ package common;
 
 import com.jme3.math.Vector3f;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by demoth on 24.04.14.
  */
@@ -27,4 +31,42 @@ public class Config {
     public static long   cl_sleep  = 50;
 
     public static float r_ambient = 1.3f;
+
+    public static interface Setter {
+        void set(String src);
+    }
+
+    public static Map<String, Setter> setters;
+
+    static {
+        setters = new HashMap<>();
+        Arrays.stream(Config.class.getDeclaredFields()).forEach(field -> {
+            if (field.getName().contains("_"))
+                setters.put(field.getName(), src -> {
+                    try {
+                        if (field.getType() == boolean.class) {
+                            field.set(null, Boolean.parseBoolean(src));
+                        } else if (field.getType() == int.class) {
+                            field.set(null, Integer.parseInt(src));
+                        } else if (field.getType() == long.class) {
+                            field.set(null, Long.parseLong(src));
+                        } else if (field.getType() == float.class) {
+                            field.set(null, Float.parseFloat(src));
+                        } else if (field.getType() == Vector3f.class) {
+                            String coords[] = src.split(" ");
+                            if (coords.length == 3)
+                                field.set(null, new Vector3f(
+                                                Float.parseFloat(coords[0]),
+                                                Float.parseFloat(coords[1]),
+                                                Float.parseFloat(coords[2]))
+                                );
+                        } else if (field.getType() == String.class) {
+                            field.set(null, src);
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                });
+        });
+    }
 }
